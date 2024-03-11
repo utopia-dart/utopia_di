b99604bd8d7f986535c98d12081758f2969b2a19
Damodar Lohani <dlohani48@gmail.com>
2024-03-11 16:59:25 +0545

    fixes and docs

---
+++ lib/src/resource_callback.dart
@@ -0,0 +1,18 @@
+
+class ResourceCallback {
+  final String name;
+  final List<String> injections;
+  final Function callback;
+  final bool reset;
+
+  ResourceCallback(
+    this.name,
+    this.injections,
+    this.callback, {
+    this.reset = false,
+  });
+
+  ResourceCallback copyWith({bool? reset}) {
+    return ResourceCallback(name, injections, callback, reset: reset ?? false);
+  }
+}
